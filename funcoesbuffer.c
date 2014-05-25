f0dbe44c469e23b588868a8248504064af3060c6
Laaia <Laaia@hotmail.com>
2014-05-25 18:49:02 -0300

    Correção na estrutura para biblioteca

--- funcoesbuffer.c
+++ funcoesbuffer.c
@@ -1,42 +1,4 @@
-#define SIZE 512
-#define PAGES 1024
-#define TAM 20
-
-int page_avalible; // Variavel global controlando as páginas utilizadas
-
-typedef struct table_bd{
-	char nome[TAM];
-	char tipo;
-	int tam;
-}tp_table;
-
-typedef struct buffer{
-   unsigned char db; //Dirty bit
-   unsigned char pc; //Pin counter
-   unsigned int nrec; //numero de registros armazenados na pagina
-   char data[SIZE];
-}tp_buffer;
-
-union c_double{
-	
-	double dnum;
-	char double_cnum[sizeof(double)];	
-};
-
-union c_int{
-	
-	int  num;
-	char cnum[sizeof(int)];
-};
-
-void cpystr(char *, char *, int, int);
-void initbuffer(tp_buffer *);
-void cria_campo(int, int, char *);
-void drawline(tp_buffer *, tp_table *, int, int *, int);
-int cabecalho(tp_table *, int);
-int printbufferpoll(tp_buffer *, tp_table *, int, int);
-int load_metadata(FILE *, tp_table *, int);
-void load_data(FILE *, tp_buffer *, int);
+#include "buffend.h"
 
 void cpystr(char *tg, char *sc, int st, int len) //SC = Ponteiro para string que carregada do arquivo; tg = Ponteiro para uma página do buffer
 {
